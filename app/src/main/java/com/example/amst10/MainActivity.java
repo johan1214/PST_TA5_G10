e96fe6bfd6e25e05dde6caf1ba14dd177a3ebe5a
kennethpro19 <krmacas@espol.edu.ec>
2021-08-05 00:16:35 -0500

    Splash Screen

--- app/src/main/java/com/example/amst10/MainActivity.java
+++ app/src/main/java/com/example/amst10/MainActivity.java
@@ -8,7 +8,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_AMST10);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
